7af093561c0a7567403e2a60ed820f0d88d51412
Ale <156391201+AleRodriguezCruz@users.noreply.github.com>
2025-01-14 10:21:02 -0800

    Update about.php

--- about.php
+++ about.php
@@ -9,38 +9,36 @@
             <div class="page-title">
             <br>
                 <div class="row">
-            	    <div class="col-md-6 col-md-offset-3">
+                    <div class="col-md-6 col-md-offset-3">
                         <h1>Acerca del autor</h1>
-                        <p>Hola, soy <strong><a target="_blank" href="http://github.com/amnersaucedososa">Amner Saucedo Sosa</a></strong> (Abizoft) creador de <a>ticketly</a>.</p>
+                        <p>Hola, soy <strong><a target="_blank" href="http://github.com/alejandrarodriguez">Alejandra Rodríguez</a></strong>, creadora de <a></a>.</p>
 
                         <br><br>
 
                         <h2> <small>Sistema de Tickets de Soporte</small></h2>
                        
-
                         <h1 style="text-align: center;"></h1>
-                        <h3>Descripcion</h3>
-                        <p>Ticketly es un sistema para gestionar tickets de soporte usando proyectos, categorias, prioridades y mas, ideal para todo tipo de negocios, desarrollo de proyectos, gestion personal, etc, el sistema esta desarrollado con PHP y MySQL.</p>
-                        <p>TICKETLY es un sistema de tickets de soporte en el que puedes rellenar tu perfil, agregar ticket, proyectos y categorias, generar reportes y mucho mas.</p>
+                        <h3>Descripción</h3>
+                        <p> es un sistema para gestionar tickets de soporte usando proyectos, categorías, prioridades y más, ideal para todo tipo de negocios, desarrollo de proyectos, gestión personal, etc. El sistema está desarrollado con PHP y MySQL.</p>
+                        <p> es un sistema de tickets de soporte en el que puedes rellenar tu perfil, agregar ticket, proyectos y categorías, generar reportes y mucho más.</p>
 
                         <h3>Dedicatoria</h3>
-                        <p>Este sistema se lo dedico a mi madre por que desde que empece en el mundo de las computadoras ella me dio su apoyo emocional y economico, me compro computadoras, me pagaron mis estudios, estoy muy agradecido con ella, la amo.</p>
+                        <p>Este sistema se lo dedico a mis padres porque desde que empecé en el mundo de las computadoras ellos me apoyaron emocional y económicamente.  Los amo.</p>
 
-                        <h3>Modulos</h3>
-                        <p>Defino a grandes rasgos los modulos generales del sistema</p>
+                        <h3>Módulos</h3>
+                        <p>Defino a grandes rasgos los módulos generales del sistema:</p>
                         <ul>
-                        <li><b>Usuarios</b>: Se puede agregar editar y eliminar usuarios.</li>
+                        <li><b>Usuarios</b>: Se puede agregar, editar y eliminar usuarios.</li>
                         <li><b>Tickets</b>: Cada usuario puede agregar un ticket.</li>
                         <li><b>Proyectos</b>: Cada usuario puede agregar los proyectos que desee.</li>
-                        <li><b>Perfiles</b>: Los usuarios pueden rellenar su perfil</li>
-                        <li><b>Categorias</b>: Los usuarios agregar categorias.</li>
+                        <li><b>Perfiles</b>: Los usuarios pueden rellenar su perfil.</li>
+                        <li><b>Categorías</b>: Los usuarios pueden agregar categorías.</li>
                         <li><b>Reportes</b>: Los usuarios pueden generar reportes.</li>
                         </ul>
 
                         <br><br>
-                       
 
-                        <h1 style="text-align: center;"><a href="http://github.com/amnersaucedososa" target="_blank">ABISOFT</a></h1>
+                        <h1 style="text-align: center;"><a href="http://github.com/alejandrarodriguez" target="_blank"></a></h1>
                     </div>
                 </div>
             </div>
